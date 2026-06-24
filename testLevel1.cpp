f7ee7650c3b210c244e15ec137115bb6c2b3839f
sliliyMiskates <131972011+sliliyMiskates@users.noreply.github.com>
2026-06-25 02:49:03 +0600

    Update testLevel1.cpp

--- testLevel1.cpp
+++ testLevel1.cpp
@@ -1,75 +1,12 @@
 #include<bits/stdc++.h>
 #include "raylib.h"
 using namespace std;
-//#include "raymath.h"
-
-//defination
-enum TileType {
-    TILE_EMPTY=0,
-    TILE_WALL=1,
-    TILE_SPEOBJECT=2,
-    TILE_CAGE=3
-};
-//dec
-struct Room {
-    std::string name;
-    int x;
-    int y;
-    int w;
-    int h;
-    Color color;
-};
 struct LevelData {
-    int id;
-    std::string name;
-    int width;
-    int height;
-    float startX;
-    float startY;
     std::vector<std::vector<int>>grid;
-    std::vector<Room>rooms;
-    Vector2 startPos;
-    Vector2 key1Pos;
-    Vector2 key2Pos;
-    bool key2FromBoss;
-    bool key1Collected;
-    bool key2Collected;
-    bool chestUnlocked;
-    Vector2 doorPos;
-    Vector2 chestPos;
-
 };
-
-vector<LevelData> maplist;
 LevelData level1;
 
 void Levelconfig() {
-    maplist.clear();
-
-    //Level 1
-    //LevelData level1;
-    level1.id=1;
-    level1.name="Catacombs of the Fallen";
-    level1.width=1024;
-    level1.height=640;
-    level1.startX=480.0f; //start from (15,14)
-    level1.startY=448.0f;
-    level1.startPos={480.0f,448.0f};
-    level1.key1Pos={192.0f,64.0f}; //one key will be in The Bone Garden(6,2)
-    level1.key2Pos={864.0f,480.0f}; //another key in Altar of First Desecration(27,15)
-    level1.doorPos={928.0f,96.0f}; //door in Portal to Cursed Depths(29,3)
-    level1.chestPos={576.0f,320.0f}; //chest in Portal to Cursed Depths(18,10)
-
-
-    level1.rooms = {
-        {"The Bone Garden of Fallen Warriors", 1, 2, 12, 5, {80, 200, 100, 75}},
-        {"Portal to Cursed Depths", 15, 2, 10, 4, {200, 50, 50, 75}},
-        {"Vault of Sealed Sorrows", 1, 8, 12, 6, {150, 40, 40, 80}},
-        {"Hall of Echoing Silence", 10, 12, 13, 6, {100, 200, 80, 75}},
-        {"Altar of First Desecration", 24, 7, 7, 11, {50, 100, 200, 75}},
-    };
-        
-    //0=blank,1=block,2=Blood Altar/Torture Device,3=cursed items,4=human bones
 
     int grid1[20][32]={
     //   0 1 2 3 4 5 6 7 8 910111213141516171819202122232425262728293031
@@ -99,14 +36,11 @@ void Levelconfig() {
         for(int c=0;c<32;c++) 
         level1.grid[r][c]=grid1[r][c];
     }
-        maplist.push_back(level1);
-
 }
 
 
 int main() 
 {
-    
     constexpr int screenWidth = 800;
     constexpr int screenHeight = 600;
     int tileSize = 25;
@@ -118,7 +52,6 @@ int main()
     
     while (!WindowShouldClose())
     {
-        
         BeginDrawing();
             
             ClearBackground(BLACK);
