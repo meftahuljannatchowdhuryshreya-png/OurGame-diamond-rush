415786debe677f35fd74337b343494fa533cc23a
meftahuljannatchowdhuryshreya-png <meftahuljannatchowdhuryshreya@gmail.com>
2026-07-18 17:17:20 +0600

    Refactor tiled map loading and drawing logic

--- Fight Sequence Version 0/src/tiledmap.cpp
+++ Fight Sequence Version 0/src/tiledmap.cpp
@@ -5,16 +5,10 @@
 #include <fstream>
 #include <vector>
 
-
 using json = nlohmann::json;
 
-
 Texture2D tileset;
-
-
-std::vector<int> tiles;
-
-
+std::vector<std::vector<int>> tiles;
 int mapWidth;
 int mapHeight;
 
@@ -22,104 +16,102 @@ int tileSize = 32;
 
 int columns;
 
-
-
 void LoadTiledMap(const char* filename)
 {
 
     std::ifstream file(filename);
 
-
     json data;
 
     file >> data;
-
-
 
     mapWidth = data["width"];
 
     mapHeight = data["height"];
 
-
-
     tileset = LoadTexture(
         "assets/tilesets/ChatGPT Image Jul 17, 2026, 10_46_00 PM.png"
     );
 
-
-
     columns = tileset.width / tileSize;
 
-
-
-    auto layer = data["layers"][0];
-
-
-    for(auto t : layer["data"])
+    // Load all tile layers
+    for(auto layer : data["layers"])
     {
-        tiles.push_back(t);
+        // Ignore object layers
+        if(!layer.contains("data"))
+            continue;
+
+
+        std::vector<int> layerTiles;
+
+
+        for(auto t : layer["data"])
+        {
+            layerTiles.push_back(t);
+        }
+
+        tiles.push_back(layerTiles);
     }
 
 }
-
-
 
 void DrawTiledMap()
 {
 
-    for(int y=0;y<mapHeight;y++)
+    for(int y = 0; y < mapHeight; y++)
     {
 
-        for(int x=0;x<mapWidth;x++)
+        for(int x = 0; x < mapWidth; x++)
         {
 
-
-            int gid =
-            tiles[y*mapWidth+x];
-
-
-            if(gid==0)
-                continue;
-
-
-
-            int id = gid - 1;
-
-
-
-            Rectangle source =
+            // Draw every layer
+            for(auto &layer : tiles)
             {
-                (float)((id%columns)*tileSize),
-                (float)((id/columns)*tileSize),
-                tileSize,
-                tileSize
-            };
+
+                int gid = layer[y * mapWidth + x];
+
+                if(gid == 0)
+                    continue;
+
+                int id = gid - 1;
+
+                Rectangle source =
+                {
+                    (float)((id % columns) * tileSize),
+                    (float)((id / columns) * tileSize),
+                    tileSize,
+                    tileSize
+                };
 
 
-            Rectangle dest =
-            {
-                x*tileSize,
-                y*tileSize,
-                tileSize,
-                tileSize
-            };
+
+                Rectangle dest =
+                {
+                    (float)(x * tileSize),
+                    (float)(y * tileSize),
+                    tileSize,
+                    tileSize
+                };
 
 
-            DrawTexturePro(
-                tileset,
-                source,
-                dest,
-                {0,0},
-                0,
-                WHITE
-            );
 
+                DrawTexturePro(
+                    tileset,
+                    source,
+                    dest,
+                    {0,0},
+                    0,
+                    WHITE
+                );
+
+            }
 
         }
+
     }
 
 }
-
 
 void UnloadTiledMap()
 {
